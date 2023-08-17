fad663234d2948b3dffed0bdad01d079c5cbc505
Jonathan Taylor <jonathan.taylor@stanford.edu>
2023-08-17 11:08:27 -0700

    glmnet_fit running

--- glmnet/_utils.py
+++ glmnet/_utils.py
@@ -1,13 +1,18 @@
 import numpy as np
+import scipy.sparse
+import statsmodels.api as sm
 
 def _get_limits(lower_limits,
                 upper_limits,
                 nvars,
                 big):
-    if lower_limits == -np.inf:
+    lower_limits = np.asarray(lower_limits)
+    upper_limits = np.asarray(upper_limits)
+    
+    if lower_limits.shape in [(), (1,)]:
         lower_limits = -np.inf * np.ones(nvars)
 
-    if upper_limits == np.inf:
+    if upper_limits.shape in [(), (1,)]:
         upper_limits = np.inf * np.ones(nvars)
 
     lower_limits = lower_limits[:nvars]
@@ -27,12 +32,18 @@ def _get_limits(lower_limits,
 def _get_vp(penalty_factor,
             exclude,
             nvars):
+
+    if penalty_factor is None:
+        penalty_factor = np.ones(nvars)
+        
     # check and standardize penalty factors (to sum to nvars)
     _isinf_penalty = np.isinf(penalty_factor)
     if np.any(_isinf_penalty):
         exclude.extend(np.nonzero(_isinf_penalty)[0])
         exclude = np.unique(exclude)
 
+    exclude = np.asarray(exclude, int)
+        
     if exclude.shape[0] > 0:
         if exclude.max() >= nvars:
             raise ValueError("Some excluded variables out of range")
@@ -128,4 +139,82 @@ def _dev_function(y,
                   weights,
                   family):
     return np.sum(family.resid_dev(y, mu, weights))
+
+def _get_start(X,
+               X_scale,
+               y,
+               weights,
+               family,
+               intercept,
+               is_offset,
+               offset,
+               exclude,
+               vp,
+               alpha): 
+
+    nobs, nvars = X.shape
+
+    # compute mu and null deviance
+    # family = binomial() gives us warnings due to non-integer weights
+    # to avoid, suppress warnings
+    if intercept:
+        if is_offset:
+            fit = sm.GLM(y,
+                         np.ones((y.shape,1)),
+                         family,
+                         offset=offset,
+                         var_weights=weights)
+            mu = fit.fitted
+        else:
+            mu = np.ones(y.shape) * (weights * y).sum() / weights.sum()
+
+    else:
+        mu = family.link.inverse(offset)
+
+    nulldev = _dev_function(y, mu, weights, family)
+
+    # if some penalty factors are zero, we have to recompute mu
+
+    vp_zero = sorted(set(exclude).difference(np.nonzero(vp == 0)[0]))
+    if vp_zero:
+        tempX = X[:,vp_zero]
+
+        if scipy.sparse.issparse(X):
+            tempX = X.toarray()
+
+        if intercept:
+            tempX = sm.add_constant(tempX)
+
+        tempfit = sm.GLM(y,
+                         tempX,
+                         family,
+                         offset=offset,
+                         var_weights=weights)
+        mu = tempfit.fittedvalues
+
+    # compute lambda max
+    ju = np.ones(nvars)
+    ju[exclude] = 0 # we have already included constant variables in exclude
+
+    r = y - mu
+    eta = family.link(mu)
+    v = family.variance(mu)
+    m_e = family.link.inverse_deriv(eta)
+    weights = weights / weights.sum()
+
+    rv = r / v * m_e * weights
+
+    if scipy.sparse.issparse(X):
+        xm, xs = X_scale
+        g = np.abs(X.T @ rv - np.sum(rv) * xm / xs)
+    else:
+        g = np.abs(X.T @ rv)
+
+    g = g * ju / (vp + (vp <= 0))
+    lambda_max = np.max(g) / max(alpha, 1e-3)
+
+    return {'nulldev':nulldev,
+            'mu':mu,
+            'lambda_max':lambda_max}
+
 
