299befb69b6ca0d2aaa22c662dc681cd8c291294
Jonathan Taylor <jonathan.taylor@stanford.edu>
2023-09-12 22:42:32 -0700

    right censored cox deviance, grad, hessian

--- glmnet/coxdev.py
+++ glmnet/coxdev.py
@@ -34,26 +34,27 @@ class right_censored(object):
         self.data = self.data.sort_values(by=['event_time', 'status'],
                                           ascending=[True, False])
         self._order = np.asarray(self.data.index)
+        self.data = self.data.reset_index()
+        _events = self.data['status'] == 1
+        first_idx, ties = _fid(self.data.loc[_events, 'event_time'], np.arange(self.data.shape[0])[_events])
 
-        first_idx, ties = _fid(self.data['event_time'])
-        _event = np.zeros_like(self.data['event_time'])
-        _event[first_idx] = 1
-
-        _weight_sum = self.data[['event_time', 'sample_weight']].groupby('event_time').sum()['sample_weight']
-        _derived_weight = np.zeros(self.data.shape[0])
+        _weight_sum = self.data.loc[_events, ['event_time', 'sample_weight']].groupby('event_time').sum()['sample_weight']
+        _derived_weight = self.data['sample_weight'].copy()
         _derived_weight[first_idx] = _weight_sum
         self.data['_derived_weight'] = _derived_weight
 
         # compute saturated loglikelihood
 
         _weight_sum = _weight_sum[_weight_sum>0]
-        self._loglik_sat = np.sum(_weight_sum * np.log(_weight_sum))
+        self._loglik_sat = - np.sum(_weight_sum * np.log(_weight_sum))
 
-        _derived_death = np.zeros(self.data.shape[0], bool)
-        _derived_death[first_idx] = 1
-        self.data['_derived_death'] = _derived_death
-        self.data['_risk_count'] = np.cumsum(self.data['_derived_death'])
-
+        _derived_events = self.data['status'].copy()
+        for v in ties.values():
+            _derived_events[v] = 0
+        _derived_events[first_idx] = 1
+        self.data['_derived_events'] = _derived_events
+        self.data['_risk_count'] = np.cumsum(self.data['_derived_events'])
+        
     def __call__(self,
                  linear_predictor,
                  gradient=True,
@@ -67,41 +68,36 @@ class right_censored(object):
         exp_eta = np.exp(eta)
 
         _derived_w = self.data['_derived_weight']
-        _derived_d = self.data['_derived_death']
+        _derived_d = self.data['_derived_events']
         rskden = np.cumsum((exp_eta*w)[::-1])[::-1]
-
-        # # compute saturated loglikelihood
-
-        # events = d==1
-        # wd = w[events]
-        # tyd = self.data['event_time'][events]
 
         log_terms = (_derived_w * np.log(rskden))[_derived_d > 0]
         loglik = np.sum((w * eta)[d > 0]) - np.sum(log_terms)
 
         if gradient or hessian:
             rskdeninv = np.hstack([0, np.cumsum((_derived_w / rskden)[_derived_d==1])])
-
             if gradient:
-                grad = w * (d - exp_eta * rskdeninv[rskcount])
-                grad[self._order] = grad
+                grad = np.array(w * (d - exp_eta * rskdeninv[rskcount]))
+                grad_cp = np.empty_like(grad)
+                grad_cp[self._order] = grad
             else:
-                grad = None
+                grad_cp = None
 
             if hessian:
                 rskdeninv2 = np.cumsum((_derived_w/(rskden**2))[_derived_d==1])
                 rskdeninv2 = np.hstack([0, rskdeninv2])
                 w_exp_eta = w * exp_eta
                 diag_hessian = w_exp_eta**2 * rskdeninv2[rskcount] - w_exp_eta * rskdeninv[rskcount]
-                diag_hessian[self._order] = diag_hessian
+                diag_hessian_cp = np.empty_like(diag_hessian)
+                diag_hessian_cp[self._order] = diag_hessian
             else:
                 diag_hessian = None
 
         return CoxDevianceResult(deviance=2*(self._loglik_sat - loglik),
-                                 grad=2*grad,  # 2 for deviance, though gets cancelled by diag_hessian...
-                                 diag_hessian=diag_hessian)
+                                 grad=2*grad_cp,  # 2 for deviance, though gets cancelled by diag_hessian...
+                                 diag_hessian=diag_hessian_cp*2)
     
-def _fid(times):
+def _fid(times, index=None):
 
     '''
     >>> fid([1,4,5,6])
@@ -111,16 +107,19 @@ def _fid(times):
     >>>
     '''
 
+    if index is None:
+        index = np.arange(times.shape[0])
+
     times = np.asarray(times)
-    unique_times, idx, counts = np.unique(times,
-                                          return_index=True,
-                                          return_counts=True)
+    unique_times, first_idx, counts = np.unique(times,
+                                                return_index=True,
+                                                return_counts=True)
     indices = {}
     for t, c in zip(unique_times,
                     counts):
         if c > 1:
-            indices[t] = np.nonzero(times == t)[0]
-    return idx, indices
+            indices[t] = index[np.nonzero(times == t)[0]]
+    return index[first_idx], indices
 
 if __name__ == "__main__":
 
