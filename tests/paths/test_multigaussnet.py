1b36612a3d446acbb108dbb4bc68f5c70997c7e1
Jonathan Taylor <jonathan.taylor@stanford.edu>
2023-10-02 11:13:16 -0700

    testing options, offset and limits matching is slightly lower

--- tests/paths/test_multigaussnet.py
+++ tests/paths/test_multigaussnet.py
@@ -1,72 +1,87 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.15.0
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 import numpy as np
+
+import pytest
 rng = np.random.default_rng(0)
-n, p, q = 200, 8, 3
-X = rng.standard_normal((n, p))
-Y = rng.standard_normal((n, q))
+
+import rpy2.robjects as rpy
+from rpy2.robjects.packages import importr
+from rpy2.robjects import numpy2ri
+from rpy2.robjects import default_converter
+
+np_cv_rules = default_converter + numpy2ri.converter
 
 from glmnet import MultiGaussNet
-nlambda = 7
-W = np.ones(n)
-W[:10] = 0
-L = MultiGaussNet(nlambda=nlambda)
-L.fit(X, Y, sample_weight=W)
 
+def get_glmnet_soln(X,
+                    Y,
+                    standardize_response=True,
+                    standardize=True,
+                    fit_intercept=True):
 
-import rpy2
-# %load_ext rpy2.ipython
-# %R -i X,Y,nlambda,W
+    with np_cv_rules.context():
 
-X[0]
+        if standardize_response:
+            rpy.r.assign('standardize.response', True)
+        else:
+            rpy.r.assign('standardize.response', False)
 
-# + language="R"
-# library(glmnet)
-# G = glmnet(X, Y, family='mgaussian', nlambda=nlambda, weights=W)
-# plot(G)
-# G
+        if standardize:
+            rpy.r.assign('standardize', True)
+        else:
+            rpy.r.assign('standardize', False)
 
-# +
-#ax = L.plot_coefficients(xvar='norm')
-# -
+        if fit_intercept:
+            rpy.r.assign('intercept', True)
+        else:
+            rpy.r.assign('intercept', False)
 
-L.summary_
+        rpy.r.assign('X', X)
+        rpy.r.assign('Y', Y)
+        rpy.r('''
+    library(glmnet)
+    X = as.matrix(X)
+    Y = as.matrix(Y)    
+    print(dim(X))
+    print(dim(Y))
+    G = glmnet(X, Y, standardize=standardize, intercept=intercept,
+               family='mgaussian',
+               standardize.response=standardize.response)
+    ''')
+        rpy.r('C = coef(G)')
 
-# + magic_args="-o C_R1,C_R2,C_R3,I_R1,I_R2,I_R3" language="R"
-# C_R1 = as.matrix(coef(G)$y1)[-1,]
-# C_R2 = as.matrix(coef(G)$y2)[-1,]
-# C_R3 = as.matrix(coef(G)$y3)[-1,]
-# I_R1 = as.matrix(coef(G)$y1)[1,]
-# I_R2 = as.matrix(coef(G)$y2)[1,]
-# I_R3 = as.matrix(coef(G)$y3)[1,]
-#
-# -
+        C = []
+        for v in range(Y.shape[1]):
+            rpy.r(f'C{v} = as.matrix(C$y{v+1})')
+            C.append(rpy.r(f'C{v}'))
+    C = np.asarray(C)
+    return np.transpose(C, [2, 1, 0])
 
-C_R = np.concatenate([C_R1.T[:,:,None], C_R2.T[:,:,None], C_R3.T[:,:,None]], axis=2)
-C_R.shape
+@pytest.mark.parametrize('standardize_response', [True, False])
+@pytest.mark.parametrize('standardize', [True, False])
+@pytest.mark.parametrize('fit_intercept', [True, False])
+@pytest.mark.parametrize('n', [1000,50])
+@pytest.mark.parametrize('p', [10,100])
+@pytest.mark.parametrize('q', [3,1])
+def test_multigaussnet(standardize_response,
+                       standardize,
+                       fit_intercept,
+                       n,
+                       p,
+                       q):
 
-np.linalg.norm(C_R - L.coefs_) / np.linalg.norm(L.coefs_)
+    X = rng.standard_normal((n, p))
+    Y = rng.standard_normal((n, q))
+    L = MultiGaussNet(standardize_response=standardize_response,
+                      standardize=standardize,
+                      fit_intercept=fit_intercept)
+    L.fit(X, Y)
+    C = get_glmnet_soln(X,
+                        Y,
+                        standardize_response=standardize_response,
+                        standardize=standardize,
+                        fit_intercept=fit_intercept)
 
-I_R = np.array([I_R1,
-                I_R2,
-                I_R3])
-np.linalg.norm(I_R.T - L.intercepts_) / np.linalg.norm(L.intercepts_)
-
-L.intercepts_.shape, L.coefs_.shape
-
-I_R.T
-
+    assert np.linalg.norm(C[:,1:] - L.coefs_) / np.linalg.norm(L.coefs_) < 1e-10
+    if fit_intercept:
+        assert np.linalg.norm(C[:,0] - L.intercepts_) / np.linalg.norm(L.intercepts_) < 1e-10
 
