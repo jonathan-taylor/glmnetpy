622071ee19cf84778fa0a48dc04e66c47849860e
Jonathan Taylor <jonathan.taylor@stanford.edu>
2025-07-03 16:35:01 -0700

    submodule checkout

--- glmnet/scoring.py
+++ glmnet/scoring.py
@@ -8,8 +8,6 @@ from sklearn.metrics import (mean_squared_error,
                              roc_auc_score,
                              average_precision_score)
 
-from .docstrings import add_dataclass_docstring
-
 @dataclass(frozen=True)
 class Scorer(object):
 
